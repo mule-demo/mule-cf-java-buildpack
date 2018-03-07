9998e1f2d6b358745fba3233485e66a45618e844
Jamie Zhang <akjamie.zhang@gmail.com>
2018-03-07 16:51:32 +0800

    initial commit

---
+++ resources/mule/bin/register_and_start.rb
@@ -0,0 +1,190 @@
+require 'json'
+require 'fileutils'
+require 'open3'
+
+#parse the VCAP APPLICATION ENV VAR SO WE HAVE THE INFO WE NEED
+appData = JSON.parse(ENV['VCAP_APPLICATION'])
+
+#todo read variables from environment
+USER = ENV['ANYPOINT_USERNAME']
+PASS = ENV['ANYPOINT_PASSWORD']
+ANYPOINT = ENV['ANYPOINT_ARM_HOST']
+ENVIRONMENT = ENV['ANYPOINT_ENVIRONMENT']
+SERVER_NAME = "#{appData['application_name']}#{ENV['CF_INSTANCE_INDEX']}"
+JAVA_HOME = ENV['JAVA_HOME']
+ANYPOINT_ON_PREM = ENV['ANYPOINT_ARM_ONPREM']
+JAVA_OPTS = ENV['JAVA_OPTS']
+  
+  
+SCRIPT_FOLDER = File.expand_path(File.dirname(__FILE__))
+
+#utility function
+def shell(*args)
+	Open3.popen3(*args) do |_stdin, stdout, stderr, wait_thr|
+		while line = stdout.gets
+	    	puts line
+	  	end
+		if wait_thr.value != 0
+		  puts "\nCommand '#{args.join ' '}' has failed"
+		  puts "STDOUT: #{stdout.gets nil}"
+		  puts "STDERR: #{stderr.gets nil}"
+
+		  fail
+		end
+	end
+end
+
+def register
+	puts "Logging into the platform..."
+
+	json = `curl -k -s -X POST 'https://#{ANYPOINT}/accounts/login?username='#{USER}'&password='#{PASS}`
+
+	if json.eql? "Unauthorized"
+		puts "Authentication failed..."
+		exit 1
+	end
+
+	#parse the response.
+	json = JSON.parse(json)
+
+	#this is the access token for the API
+	access_token = json['access_token']
+
+	#build a header for the token
+	token_header = "-H \"Authorization: Bearer #{access_token}\""
+
+
+
+	#learn which is the organization ID of the current user
+	puts "Getting the current org id..."
+	json = `curl -k -s -X GET #{token_header} https://#{ANYPOINT}/accounts/api/me`
+
+	json = JSON.parse(json)
+
+	org_id =  json['user']['organization']['id']
+
+	#build a header for the organization id
+	org_header = "-H \"X-ANYPNT-ORG-ID: #{org_id}\""
+
+	#get the current environement id.
+	puts "Getting the id for the selected environment..."
+	json = `curl -k -s -X GET #{token_header} https://#{ANYPOINT}/accounts/api/organizations/#{org_id}/environments`
+
+	json = JSON.parse(json)
+
+	env_id = nil
+
+	json['data'].each do |env| 
+		if env['name'].eql? ENVIRONMENT
+			env_id = env['id']
+			break
+		end
+	 end
+
+	#build a header for the environment id.
+	env_header = "-H \"X-ANYPNT-ENV-ID: #{env_id}\""
+
+
+	########## At this point we can check if the server exists and if it does, delete it. #######
+	puts "Looking for servers with the same name..."
+
+	json = `curl -k -s -X GET #{token_header} #{org_header} #{env_header} https://#{ANYPOINT}/hybrid/api/v1/servers`
+
+
+	json = JSON.parse(json)
+
+	json['data'].each do |srv|
+	    
+	    if srv['name'].eql? SERVER_NAME
+	        puts "Found server with name: #{SERVER_NAME}, attempting to clear it ..."
+	        `curl -k -s -X DELETE #{token_header} #{org_header} #{env_header} https://#{ANYPOINT}/hybrid/api/v1/servers/#{srv['id']}`
+	        break
+	    end
+	end
+
+	###### At this point we can get the registration token #######
+	puts "Getting registration token..."
+	json = `curl -k -s -X GET #{token_header} #{org_header} #{env_header} https://#{ANYPOINT}/hybrid/api/v1/servers/registrationToken`
+
+	json = JSON.parse(json)
+
+	reghash = json['data']
+
+	#Run the server registration script...
+	if ANYPOINT_ON_PREM.nil? || ANYPOINT_ON_PREM.empty?
+
+	  cmd = [
+	      "export",
+	      "JAVA_HOME=#{JAVA_HOME}",
+	      "&&",
+	      "#{SCRIPT_FOLDER}/amc_setup",
+	      "-H",
+	      reghash,
+	      "#{SERVER_NAME}"
+	    ].flatten.compact.join(' ')
+	else
+	    #this is the command that needs to be used with arm on prem
+	    cmd = [
+	        "export",
+	        "JAVA_HOME=#{JAVA_HOME}",
+	        "&&",
+	        "#{SCRIPT_FOLDER}/amc_setup",
+	        "-A http://#{ANYPOINT}:8080/hybrid/api/v1",
+	        "-W \"wss://#{ANYPOINT}:8443/mule\"",
+	        "-F https://#{ANYPOINT}/apiplatform",
+	        "-C https://#{ANYPOINT}/accounts",
+	        "-H",
+	        reghash,
+	        "#{SERVER_NAME}"
+	      ].flatten.compact.join(' ')
+	end
+
+	puts "Running registration..."
+	puts `#{cmd}`
+end
+
+def run
+	################### FINALLY RUN THE MULE #####################
+	
+	mem = ENV['MEMORY_LIMIT'].chomp("m").to_i / 2
+
+
+	isUsingAppDynamics = JAVA_OPTS.include?("-Dappdynamics.")
+	  
+	cmd = [
+	    "export",
+	    "JAVA_HOME=#{JAVA_HOME}",
+	    "&&",
+		"#{SCRIPT_FOLDER}/gateway",
+	    "wrapper.java.maxmemory=#{mem}",
+	    "wrapper.java.initmemory=#{mem}",
+	    (isUsingAppDynamics ? ("-M-javaagent:./.java-buildpack/app_dynamics_agent/javaagent.jar"):("") ),
+	    "-M-Dmule.agent.enabled=false",
+	    "-M-Danypoint.platform.client_id=$ANYPOINT_PLATFORM_CLIENT_ID",
+      "-M-Danypoint.platform.client_secret=$ANYPOINT_PLATFORM_CLIENT_SECRET",
+      "-M-Danypoint.platform.platform_base_uri=$ANYPOINT_PLATFORM_BASE_URI",
+      "-M-Danypoint.platform.coreservice_base_uri=$ANYPOINT_PLATFORM_CORESERVICE_BASE_URI",
+	    "-M-Dhttp.port=$PORT",
+	    JAVA_OPTS.gsub('-D', '-M-D')
+	 ].flatten.compact.join(' ')
+
+#	 
+#  expect(java_opts).to include('-javaagent:$PWD/.java-buildpack/app_dynamics_agent/javaagent.jar')
+#          expect(java_opts).to include('-Dappdynamics.controller.hostName=test-host-name')
+#          expect(java_opts).to include('-Dappdynamics.agent.applicationName=test-application-name')
+#          expect(java_opts).to include('-Dappdynamics.agent.tierName=test-application-name')
+#          expect(java_opts).to include('-Dappdynamics.agent.nodeName=$(expr "$VCAP_APPLICATION" : ' \
+#                                       '\'.*instance_index[": ]*\\([[:digit:]]*\\).*\')')
+	 
+	puts "Running mule..."
+	puts cmd
+	shell cmd
+end
+
+
+
+if !ANYPOINT.nil?
+	register
+end
+
+run
